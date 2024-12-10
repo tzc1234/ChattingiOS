096796e76995eb1fd38a3b816cda700ad47b92ac
tzc1234 <peopli9997@gmail.com>
2024-12-10 14:55:26 +0000

    Initial Commit

---
+++ ChattingiOS/ChattingiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingiOSApp.swift
+//  ChattingiOS
+//
+//  Created by Tsz-Lung on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChattingiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
