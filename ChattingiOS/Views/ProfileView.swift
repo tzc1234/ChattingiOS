218002648fa7e29871f980d8ddbf1d73952d396a
tzc1234 <peopli9997@gmail.com>
2024-12-11 16:03:12 +0000

    Add ProfileView

---
+++ ChattingiOS/Views/ProfileView.swift
@@ -0,0 +1,54 @@
+//
+//  ProfileView.swift
+//  ChattingiOS
+//
+//  Created by Tsz-Lung on 11/12/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        ZStack {
+            Color.orange
+            
+            VStack(spacing: 12) {
+                VStack(spacing: 2) {
+                    Image(systemName: "person.circle")
+                        .font(.system(size: 120))
+                        .foregroundStyle(.gray)
+                    
+                    VStack(spacing: 2) {
+                        Text("Abc")
+                            .font(.headline)
+                        
+                        Text("abc@email.com")
+                            .font(.subheadline)
+                    }
+                }
+                
+                Button {
+                    print("Sign Out Tapped.")
+                } label: {
+                    Text("Sign Out")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding(12)
+                        .background(.red, in: .rect(cornerRadius: 8))
+                }
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+            )
+            .padding()
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ProfileView()
+}
