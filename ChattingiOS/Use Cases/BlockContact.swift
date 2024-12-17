9c4259282fcf5ee770c3ba165ee14a0a1c1d64e4
tzc1234 <peopli9997@gmail.com>
2024-12-17 11:31:12 +0000

    Add BlockContact use case

---
+++ ChattingiOS/Use Cases/BlockContact.swift
@@ -0,0 +1,28 @@
+//
+//  BlockContact.swift
+//  ChattingiOS
+//
+//  Created by Tsz-Lung on 17/12/2024.
+//
+
+import Foundation
+
+final class BlockContact {
+    private let client: HTTPClient
+    private let getRequest: (Int) -> URLRequest
+    
+    init(client: HTTPClient, getRequest: @escaping (Int) -> URLRequest) {
+        self.client = client
+        self.getRequest = getRequest
+    }
+    
+    func block(with contactID: Int) async throws(UseCaseError) -> Contact {
+        let request = getRequest(contactID)
+        do {
+            let (data, response) = try await client.send(request)
+            return try ContactResponseMapper.map(data, response: response)
+        } catch {
+            throw .map(error)
+        }
+    }
+}
