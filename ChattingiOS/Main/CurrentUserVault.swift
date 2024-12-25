d83004e3a873a91ea05bc3897f70359ed4e2d990
tzc1234 <peopli9997@gmail.com>
2024-12-25 19:01:28 +0000

    Add support to save/retrieve token in CurrentUserVault

--- ChattingiOS/Main/CurrentUserVault.swift
+++ ChattingiOS/Main/CurrentUserVault.swift
@@ -6,8 +6,13 @@
 //
 
 import Foundation
+import Security
 
 actor CurrentUserVault {
+    private let defaults = UserDefaults.standard
+}
+
+extension CurrentUserVault {
     private struct CodableUser: Codable {
         let id: Int
         let name: String
@@ -26,18 +31,11 @@ actor CurrentUserVault {
         }
     }
     
-    private let defaults = UserDefaults.standard
-}
-
-extension CurrentUserVault {
     private static var currentUserKey: String { "current_user" }
     
-    func saveUser(_ user: User) {
+    func saveUser(_ user: User) throws {
         let codableUser = CodableUser(user)
-        guard let data = try? JSONEncoder().encode(codableUser) else {
-            return
-        }
-        
+        let data = try JSONEncoder().encode(codableUser)
         defaults.set(data, forKey: Self.currentUserKey)
     }
     
@@ -48,5 +46,95 @@ extension CurrentUserVault {
         
         let codableUser = try? JSONDecoder().decode(CodableUser.self, from: data)
         return codableUser?.user
+    }
+}
+
+extension CurrentUserVault {
+    private struct CodableToken: Codable {
+        let accessToken: String
+        let refreshToken: String
+        
+        init(_ token: Token) {
+            self.accessToken = token.accessToken
+            self.refreshToken = token.refreshToken
+        }
+        
+        var token: Token {
+            Token(accessToken: accessToken, refreshToken: refreshToken)
+        }
+    }
+    
+    enum Error: Swift.Error {
+        case saveTokenFail
+        case deleteTokenFail
+    }
+    
+    private static var tokenKey: String { "token" }
+    
+    func saveToken(_ token: Token) throws {
+        let data = try map(token)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: Self.tokenKey,
+            kSecValueData as String: data
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        if status == errSecDuplicateItem {
+            try updateToken(data: data)
+        } else if status != errSecSuccess {
+            throw CurrentUserVault.Error.saveTokenFail
+        }
+    }
+    
+    private func map(_ token: Token) throws -> Data {
+        try JSONEncoder().encode(CodableToken(token))
+    }
+    
+    private func updateToken(data: Data) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: Self.tokenKey
+        ]
+        
+        let attributes: [String: Any] = [
+            kSecValueData as String: data
+        ]
+        
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        if status != errSecSuccess {
+            throw CurrentUserVault.Error.saveTokenFail
+        }
+    }
+    
+    func retrieveToken() -> Token? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: Self.tokenKey,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess,
+              let data = item as? Data,
+              let codableToken = try? JSONDecoder().decode(CodableToken.self, from: data) else {
+            return nil
+        }
+        
+        return codableToken.token
+    }
+    
+    func deleteToken() throws {
+        let query: [String: Any] = [
+            kSecAttrAccount as String: Self.tokenKey,
+            kSecClass as String: kSecClassGenericPassword
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        if status != errSecSuccess {
+            throw CurrentUserVault.Error.deleteTokenFail
+        }
     }
 }
