ed5d7c3ca594c3debb8c534525274588a9e80ec3
tzc1234 <peopli9997@gmail.com>
2024-12-28 15:19:29 +0000

    Add DefaultGetContacts in DependenciesContainer

--- ChattingiOS/Main/DependenciesContainer.swift
+++ ChattingiOS/Main/DependenciesContainer.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 final class DependenciesContainer {
     let userVault = CurrentUserCredentialVault()
     private let httpClient = URLSessionHTTPClient(session: .shared)
@@ -16,5 +17,20 @@ final class DependenciesContainer {
     }
     private(set) lazy var userSignUp = UserSignUp(client: httpClient) {
         UserSignUpEndpoint(params: $0).request
+    }
+    
+    private(set) lazy var getContacts = DefaultGetContacts(client: httpClient) { [weak self] in
+        let accessToken = await self?.accessToken() ?? ""
+        let endpoint = GetContactsEndpoint(params: $0)
+        return AuthorizedEndpoint(accessToken: accessToken, endpoint: endpoint).request
+    }
+    
+    private func accessToken() async -> String? {
+        guard let accessToken = await userVault.retrieveToken()?.accessToken else {
+            try? await userVault.deleteUserCredential()
+            return nil
+        }
+        
+        return accessToken
     }
 }
