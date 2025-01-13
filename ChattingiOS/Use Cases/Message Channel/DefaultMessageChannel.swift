9e34a7b0e4f1db5114dc4079f54cba33c188e5c9
tzc1234 <peopli9997@gmail.com>
2025-01-13 10:25:45 +0000

    Separate WebSocketError from WebSocketClientError

--- ChattingiOS/Use Cases/Message Channel/DefaultMessageChannel.swift
+++ ChattingiOS/Use Cases/Message Channel/DefaultMessageChannel.swift
@@ -1,37 +1,11 @@
 //
-//  MessageChannel.swift
+//  DefaultMessageChannel.swift
 //  ChattingiOS
 //
 //  Created by Tsz-Lung on 18/12/2024.
 //
 
 import Foundation
-
-enum MessageChannelError: Error {
-    case invalidURL
-    case unauthorized
-    case notFound
-    case forbidden
-    case unknown
-    case disconnected
-    case unsupportedData
-    case userInitiateSignOut
-    case requestCreation
-    case other(Error)
-}
-
-protocol MessageChannel: Sendable {
-    func establish(for contactID: Int) async throws(MessageChannelError) -> MessageChannelConnection
-}
-
-protocol MessageChannelConnection: Sendable {
-    typealias MessageObserver = @Sendable (Message) async -> Void
-    typealias ErrorObserver = @Sendable (MessageChannelError) async -> Void
-    
-    func startObserving(messageObserver: MessageObserver?, errorObserver: ErrorObserver?) async
-    func send(text: String) async throws
-    func close() async throws
-}
 
 actor DefaultMessageChannel: MessageChannel {
     private let client: WebSocketClient
@@ -45,11 +19,11 @@ actor DefaultMessageChannel: MessageChannel {
     private struct Connection: MessageChannelConnection {
         private let webSocket: WebSocket
         
-        init(webSocket: WebSocket) {
+        init(_ webSocket: WebSocket) {
             self.webSocket = webSocket
         }
         
-        func startObserving(messageObserver: MessageObserver?, errorObserver: ErrorObserver?) async {
+        func start(messageObserver: MessageObserver?, errorObserver: ErrorObserver?) async {
             await webSocket.setObservers { data in
                 do {
                     let message = try MessageChannelReceivedMessageMapper.map(data)
@@ -84,7 +58,7 @@ actor DefaultMessageChannel: MessageChannel {
         
         do {
             let webSocket = try await client.connect(request)
-            return Connection(webSocket: webSocket)
+            return Connection(webSocket)
         } catch {
             throw error.toMessageChannelError
         }
@@ -104,6 +78,15 @@ private extension WebSocketClientError {
             .forbidden
         case .unknown:
             .unknown
+        case .other(let error):
+            .other(error)
+        }
+    }
+}
+
+private extension WebSocketError {
+    var toMessageChannelError: MessageChannelConnectionError {
+        switch self {
         case .disconnected:
             .disconnected
         case .unsupportedData:
