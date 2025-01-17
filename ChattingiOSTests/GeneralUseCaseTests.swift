9f18f78f2cd01d899b748b404659254aa60a13f2
tzc1234 <peopli9997@gmail.com>
2025-01-17 14:49:28 +0000

    Add makeSUT helper

--- ChattingiOSTests/GeneralUseCaseTests.swift
+++ ChattingiOSTests/GeneralUseCaseTests.swift
@@ -10,34 +10,42 @@ import XCTest
 
 @MainActor
 final class GeneralUseCaseTests: XCTestCase {
-    func test_init_doesNotNotifyClientWhileCreation() async {
-        let client = httpClientSpy()
-        let sut = GeneralUseCase<String, MapperStub>(client: client) { _ in
-            URLRequest(url: URL(string: "http://a-url.com")!)
-        }
+    func test_init_doesNotNotifyClientWhileCreation() {
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.messages.isEmpty)
-        
-        trackMemoryLeak(sut)
-        trackMemoryLeak(client)
     }
     
     // MARK: - Helpers
     
+    private typealias SUT = GeneralUseCase<String, MapperStub>
+    
+    private func makeSUT(request: sending @escaping (String) -> URLRequest = { _ in URLRequest(url: anyURL()) },
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: SUT, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = SUT(client: client, getRequest: request)
+        trackMemoryLeak(sut)
+        trackMemoryLeak(client)
+        return (sut, client)
+    }
+    
     private enum MapperStub: ResponseMapper {
-        typealias Model = String
-        
         static func map(_ data: Data, response: HTTPURLResponse) throws(MapperError) -> String {
             ""
         }
     }
     
     @MainActor
-    private final class httpClientSpy: HTTPClient {
+    private final class HTTPClientSpy: HTTPClient {
         private(set) var messages = [Any]()
         
         func send(_ request: URLRequest) async throws -> (data: Data, response: HTTPURLResponse) {
             fatalError()
         }
     }
+}
+
+func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
 }
