d1781f2c06c3925eed63b017a1c6fc2ff4569a3d
tzc1234 <peopli9997@gmail.com>
2025-01-20 16:28:04 +0000

    Change accessToken and messageChannelAccessToken in DependenciesContainer from func to var

--- ChattingiOS/Main/DependenciesContainer.swift
+++ ChattingiOS/Main/DependenciesContainer.swift
@@ -29,26 +29,26 @@ final class DependenciesContainer {
         currentUserVault: currentUserVault
     )
     
-    private(set) lazy var getContacts = DefaultGetContacts(client: refreshTokenHTTPClient) { [accessToken = accessToken()] in
+    private(set) lazy var getContacts = DefaultGetContacts(client: refreshTokenHTTPClient) { [accessToken] in
         GetContactsEndpoint(accessToken: try await accessToken(), params: $0).request
     }
-    private(set) lazy var newContact = DefaultNewContact(client: refreshTokenHTTPClient) { [accessToken = accessToken()] in
+    private(set) lazy var newContact = DefaultNewContact(client: refreshTokenHTTPClient) { [accessToken] in
         NewContactEndpoint(accessToken: try await accessToken(), responderEmail: $0).request
     }
-    private(set) lazy var getMessages = DefaultGetMessages(client: refreshTokenHTTPClient) { [accessToken = accessToken()] in
+    private(set) lazy var getMessages = DefaultGetMessages(client: refreshTokenHTTPClient) { [accessToken] in
         GetMessagesEndpoint(accessToken: try await accessToken(), params: $0).request
     }
-    private(set) lazy var readMessages = DefaultReadMessages(client: refreshTokenHTTPClient) { [accessToken = accessToken()] in
+    private(set) lazy var readMessages = DefaultReadMessages(client: refreshTokenHTTPClient) { [accessToken] in
         ReadMessagesEndpoint(accessToken: try await accessToken(), params: $0).request
     }
-    private(set) lazy var blockContact = DefaultBlockContact(client: httpClient) { [accessToken = accessToken()] in
+    private(set) lazy var blockContact = DefaultBlockContact(client: httpClient) { [accessToken] in
         BlockContactEndpoint(accessToken: try await accessToken(), contactID: $0).request
     }
-    private(set) lazy var unblockContact = DefaultUnblockContact(client: httpClient) { [accessToken = accessToken()] in
+    private(set) lazy var unblockContact = DefaultUnblockContact(client: httpClient) { [accessToken] in
         UnblockContactEndpoint(accessToken: try await accessToken(), contactID: $0).request
     }
     
-    private func accessToken() -> (@Sendable () async throws -> AccessToken) {
+    private var accessToken: (@Sendable () async throws -> AccessToken) {
         { [currentUserVault, contentViewModel] in
             guard let accessToken = await currentUserVault.retrieveCurrentUser()?.accessToken else {
                 try? await currentUserVault.deleteCurrentUser()
@@ -72,11 +72,11 @@ final class DependenciesContainer {
         currentUserVault: currentUserVault
     )
     
-    private(set) lazy var messageChannel = DefaultMessageChannel(client: refreshTokenWebSocketClient) { [accessToken = messageChannelAccessToken()] in
-        MessageChannelEndpoint(accessToken: try await accessToken(), contactID: $0).request
+    private(set) lazy var messageChannel = DefaultMessageChannel(client: refreshTokenWebSocketClient) { [messageChannelAccessToken] in
+        MessageChannelEndpoint(accessToken: try await messageChannelAccessToken(), contactID: $0).request
     }
     
-    private func messageChannelAccessToken() -> (@Sendable () async throws -> AccessToken) {
+    private var messageChannelAccessToken: (@Sendable () async throws -> AccessToken) {
         { [currentUserVault, contentViewModel] in
             guard let accessToken = await currentUserVault.retrieveCurrentUser()?.accessToken else {
                 try? await currentUserVault.deleteCurrentUser()
