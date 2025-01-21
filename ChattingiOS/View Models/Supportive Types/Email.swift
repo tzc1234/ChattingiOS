d6afdb45a4719d6652f1c94b3ec0781016e60c22
tzc1234 <peopli9997@gmail.com>
2025-01-21 13:42:51 +0000

    Change Email to a typealias of Input

--- ChattingiOS/View Models/Supportive Types/Email.swift
+++ ChattingiOS/View Models/Supportive Types/Email.swift
@@ -7,49 +7,18 @@
 
 import Foundation
 
-enum Email {
-    case wrapped(String)
-    case empty
-    case error(String)
+enum EmailValidator: Validator {
+    static var validators: [(String) -> ValidatorResult] { [validateEmpty, validateFormat] }
     
-    init(_ email: String) {
-        guard !email.isEmpty else {
-            self = .empty
-            return
-        }
-        
-        guard Self.isValid(email) else {
-            self = .error("Email format is not correct.")
-            return
-        }
-        
-        self = .wrapped(email)
-    }
-    
-    var isValid: Bool {
-        switch self {
-        case .wrapped: true
-        default: false
-        }
-    }
-    
-    var value: String? {
-        switch self {
-        case let .wrapped(string): string
-        default: nil
-        }
-    }
-    
-    var errorMessage: String? {
-        switch self {
-        case let .error(string): string
-        default: nil
-        }
-    }
-    
-    private static func isValid(_ email: String) -> Bool {
+    private static func validateFormat(_ email: String) -> ValidatorResult {
         let regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,64}$"
         let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regex)
-        return predicate.evaluate(with: email)
+        guard predicate.evaluate(with: email) else {
+            return .invalid("Email format is not correct.")
+        }
+        
+        return .valid
     }
 }
+
+typealias Email = Input<EmailValidator>
