1b9f69dd959b9d33fe0434a61b3cf7284096df5d
tzc1234 <peopli9997@gmail.com>
2025-01-22 14:29:05 +0000

    MessageChannelReceivedMessageMapper map delivers message correctly

--- ChattingiOSTests/Networkings/Mappers/MessageChannelReceivedMessageMapperTests.swift
+++ ChattingiOSTests/Networkings/Mappers/MessageChannelReceivedMessageMapperTests.swift
@@ -16,4 +16,42 @@ final class MessageChannelReceivedMessageMapperTests: XCTestCase {
             XCTAssertEqual(error as? MessageStreamError, .invalidData)
         }
     }
+    
+    func test_map_deliversMessageCorrectly() throws {
+        let expectedMessages = [
+            Message(id: 1, text: "any text", senderID: 1, isRead: false, createdAt: nil),
+            Message(id: 99, text: "another text", senderID: 99, isRead: true, createdAt: .distantFuture)
+        ]
+        
+        for expectedMessage in expectedMessages {
+            let receivedMessage = try MessageChannelReceivedMessageMapper.map(expectedMessage.toData)
+            
+            XCTAssertEqual(receivedMessage, expectedMessage)
+        }
+    }
+}
+
+private extension Message {
+    private struct MessageResponseForTest: Encodable {
+        let id: Int
+        let text: String
+        let sender_id: Int
+        let is_read: Bool
+        let created_at: Date?
+        
+        init(_ message: Message) {
+            self.id = message.id
+            self.text = message.text
+            self.sender_id = message.senderID
+            self.is_read = message.isRead
+            self.created_at = message.createdAt
+        }
+    }
+    
+    var toData: Data {
+        let response = MessageResponseForTest(self)
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        return try! encoder.encode(response)
+    }
 }
