7bd16c8af1f4fa5bde01ca282f2b9427f22d3610
tzc1234 <peopli9997@gmail.com>
2025-01-22 14:11:14 +0000

    ErrorResponseMapper map error data delivers no reason while invalid data

--- ChattingiOSTests/Networkings/Mappers/ErrorResponseMapperTests.swift
+++ ChattingiOSTests/Networkings/Mappers/ErrorResponseMapperTests.swift
@@ -9,6 +9,14 @@ import XCTest
 @testable import ChattingiOS
 
 final class ErrorResponseMapperTests: XCTestCase {
+    func test_mapErrorData_deliversNoReasonWhileInvalidData() {
+        let data = Data("invalid".utf8)
+        
+        let receivedReason = ErrorResponseMapper.map(errorData: data)
+        
+        XCTAssertNil(receivedReason)
+    }
+    
     func test_mapErrorData_deliversReasonCorrectly() {
         let expectedReason = "any error reason"
         let data = Data("{\"reason\":\"\(expectedReason)\"}".utf8)
