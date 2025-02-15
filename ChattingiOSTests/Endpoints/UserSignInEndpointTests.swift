9985649bf5267af5609713f6c11cc60dc852c06f
tzc1234 <peopli9997@gmail.com>
2025-02-15 15:50:42 +0000

    UserSignInEndpoint constructs request correctly

---
+++ ChattingiOSTests/Endpoints/UserSignInEndpointTests.swift
@@ -0,0 +1,44 @@
+//
+//  UserSignInEndpointTests.swift
+//  ChattingiOSTests
+//
+//  Created by Tsz-Lung on 15/02/2025.
+//
+
+import XCTest
+@testable import ChattingiOS
+
+final class UserSignInEndpointTests: XCTestCase {
+    func test_request_constructsRequestCorrectly() throws {
+        let endpoint = try UserSignInEndpoint(apiConstants: .test, params: param)
+        let expectedURL = APIConstants.test.url.appending(component: "login")
+        
+        let request = endpoint.request
+        
+        XCTAssertEqual(request.url, expectedURL)
+        XCTAssertEqual(request.httpMethod, "POST")
+    }
+    
+    // MARK: - Helpers
+    
+    private var param: UserSignInParams {
+        UserSignInParams(email: "any@email.com", password: "any-password")
+    }
+}
+
+extension APIConstants {
+    static var test: Self {
+        APIConstants(
+            scheme: "http",
+            webSocketScheme: "ws",
+            host: "test-host",
+            port: 81,
+            apiPath: "/api-path/"
+        )
+    }
+    
+    var url: URL {
+        let string = "\(scheme)://\(host):\(port!)\(apiPath)"
+        return URL(string: string)!
+    }
+}
