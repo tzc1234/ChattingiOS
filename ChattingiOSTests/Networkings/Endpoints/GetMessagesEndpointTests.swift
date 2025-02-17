85001e114aa7d94d54b76f517ef1ccc21f2ce9d2
tzc1234 <peopli9997@gmail.com>
2025-02-17 13:30:50 +0000

    GetMessagesEndpoint constructs URL correctly with limit

--- ChattingiOSTests/Networkings/Endpoints/GetMessagesEndpointTests.swift
+++ ChattingiOSTests/Networkings/Endpoints/GetMessagesEndpointTests.swift
@@ -12,8 +12,6 @@ final class GetMessagesEndpointTests: XCTestCase {
     func test_request_constructsURLCorrectlyWithMessageID() {
         let constants = APIConstants.test
         let token = anyAccessToken
-        let contactID = 1
-        let messageID = 99
         let params = GetMessagesParams(contactID: contactID, messageID: .before(messageID))
         let endpoint = GetMessagesEndpoint(apiConstants: constants, accessToken: token, params: params)
         
@@ -25,4 +23,25 @@ final class GetMessagesEndpointTests: XCTestCase {
         XCTAssertFalse(url.absoluteString.contains("after_message_id="))
         XCTAssertFalse(url.absoluteString.contains("limit="))
     }
+    
+    func test_request_constructsURLCorrectlyWithLimit() {
+        let constants = APIConstants.test
+        let token = anyAccessToken
+        let params = GetMessagesParams(contactID: contactID, limit: limit)
+        let endpoint = GetMessagesEndpoint(apiConstants: constants, accessToken: token, params: params)
+        
+        let request = endpoint.request
+        let url = request.url!
+        
+        XCTAssertEqual(url.withoutQuery(), constants.url(lastPart: "contacts/\(params.contactID)/messages"))
+        XCTAssertFalse(url.absoluteString.contains("before_message_id="))
+        XCTAssertFalse(url.absoluteString.contains("after_message_id="))
+        XCTAssertTrue(url.absoluteString.contains("limit=\(limit)"))
+    }
+    
+    // MARK: - Helpers
+    
+    private var contactID: Int { 1 }
+    private var messageID: Int { 99 }
+    private var limit: Int { 10 }
 }
