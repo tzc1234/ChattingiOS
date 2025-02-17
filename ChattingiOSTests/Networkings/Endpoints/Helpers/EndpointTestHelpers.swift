24a80d565711d5b16763d58cdf05c0b911aad0ee
tzc1234 <peopli9997@gmail.com>
2025-02-17 14:12:25 +0000

    Check bearer access token is not empty before construct expectedHeaderFields

--- ChattingiOSTests/Networkings/Endpoints/Helpers/EndpointTestHelpers.swift
+++ ChattingiOSTests/Networkings/Endpoints/Helpers/EndpointTestHelpers.swift
@@ -5,7 +5,7 @@
 //  Created by Tsz-Lung on 17/02/2025.
 //
 
-import Foundation
+import XCTest
 @testable import ChattingiOS
 
 var httpHeaderFields: [String: String] {
@@ -17,7 +17,14 @@ var httpHeaderFields: [String: String] {
 
 var anyAccessToken: AccessToken { AccessToken(wrappedString: "any-token") }
 
-func expectedHeaderFields(with accessToken: AccessToken) -> [String: String] {
+func expectedHeaderFields(with accessToken: AccessToken,
+                          file: StaticString = #filePath,
+                          line: UInt = #line) -> [String: String] {
+    guard !accessToken.bearerToken.isEmpty else {
+        XCTFail("Bearer access token should not be empty", file: file, line: line)
+        return [:]
+    }
+    
     var fields = httpHeaderFields
     fields["Authorization"] = accessToken.bearerToken
     return fields
