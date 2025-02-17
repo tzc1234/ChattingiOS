87ea9726d689cd84a81351fee3602d567e63446b
tzc1234 <peopli9997@gmail.com>
2025-02-17 18:43:16 +0000

    Improve the body content checking mechanism in UserSignUpEndpointTests

--- ChattingiOSTests/Networkings/Endpoints/UserSignUpEndpointTests.swift
+++ ChattingiOSTests/Networkings/Endpoints/UserSignUpEndpointTests.swift
@@ -12,20 +12,21 @@ final class UserSignUpEndpointTests: XCTestCase {
     func test_request_constructsRequestCorrectlyWithoutAvatar() throws {
         let constants = APIConstants.test
         let params = UserSignUpParams(name: "any name", email: "any@email.com", password: "any-password", avatar: nil)
-        let boundary = UUID().uuidString
         let endpoint = UserSignUpEndpoint(apiConstants: constants, boundary: boundary, params: params)
         
         let request = endpoint.request
         
         XCTAssertEqual(request.url, constants.url(lastPart: "register"))
         XCTAssertEqual(request.httpMethod, "POST")
-        XCTAssertEqual(request.allHTTPHeaderFields, expectedHeaderFields(boundary: boundary))
-        try assertBody(request.httpBody, with: params, boundary: boundary)
+        XCTAssertEqual(request.allHTTPHeaderFields, expectedHeaderFields)
+        try assertBody(request.httpBody, with: params)
     }
     
     // MARK: - Helpers
     
-    private func expectedHeaderFields(boundary: String) -> [String: String] {
+    private let boundary = UUID().uuidString
+    
+    private var expectedHeaderFields: [String: String] {
         var fields = httpHeaderFields
         fields["Content-Type"] = "multipart/form-data; boundary=\(boundary)"
         return fields
@@ -33,25 +34,28 @@ final class UserSignUpEndpointTests: XCTestCase {
     
     private func assertBody(_ data: Data?,
                             with params: UserSignUpParams,
-                            boundary: String,
                             file: StaticString = #filePath,
                             line: UInt = #line) throws {
         guard let data else { return XCTFail("Body should not be nil", file: file, line: line) }
         
-        let body = try XCTUnwrap(String(data: data, encoding: .utf8))
-        let contents = body.split(separator: "\r\n")
+        var body = try XCTUnwrap(String(data: data, encoding: .utf8))
         
-        XCTAssertEqual(contents.first, "--\(boundary)", file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == content(name: "name") }, file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == "\(params.name)" }, file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == content(name: "email") }, file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == "\(params.email)" }, file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == content(name: "password") }, file: file, line: line)
-        XCTAssertTrue(contents.contains { $0 == "\(params.password)" }, file: file, line: line)
-        XCTAssertEqual(contents.last, "--\(boundary)--", file: file, line: line)
+        body = checkAndRemoveFirst(content(name: "name", value: params.name), in: body, file: file, line: line)
+        body = checkAndRemoveFirst(content(name: "email", value: params.email), in: body, file: file, line: line)
+        body = checkAndRemoveFirst(content(name: "password", value: params.password), in: body, file: file, line: line)
+        body = checkAndRemoveFirst("--\(boundary)--\r\n", in: body, file: file, line: line)
+        XCTAssertTrue(body.isEmpty, file: file, line: line)
     }
     
-    private func content(name: String) -> String {
-        "Content-Disposition: form-data; name=\"\(name)\""
+    private func checkAndRemoveFirst(_ str: String,
+                                     in content: String,
+                                     file: StaticString = #filePath,
+                                     line: UInt = #line) -> String {
+        XCTAssertTrue(content.contains(str), "`\(str)` is not existed in body", file: file, line: line)
+        return content.replacingOccurrences(of: str, with: "")
+    }
+    
+    private func content(name: String, value: String) -> String {
+        "--\(boundary)\r\nContent-Disposition: form-data; name=\"\(name)\"\r\n\r\n\(value)\r\n"
     }
 }
