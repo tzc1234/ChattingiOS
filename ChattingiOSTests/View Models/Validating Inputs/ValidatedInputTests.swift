4344987f6135a634527fad58731bda4ce7655fab
tzc1234 <peopli9997@gmail.com>
2025-04-10 14:05:36 +0100

    ValidatedInput initialises with error message on validator failure

--- ChattingiOSTests/View Models/Validating Inputs/ValidatedInputTests.swift
+++ ChattingiOSTests/View Models/Validating Inputs/ValidatedInputTests.swift
@@ -9,20 +9,54 @@ import XCTest
 @testable import ChattingiOS
 
 final class ValidatedInputTests: XCTestCase {
-    func test_init_initialisedSuccessfullyWithValidValue() {
+    override func setUp() {
+        super.setUp()
+        
+        resetErrorMessage()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        resetErrorMessage()
+    }
+    
+    func test_init_initialisesWithValueOnValidatorSuccess() {
         let validValue = "valid value"
-        let input = ValidatedInput<AlwaysReturnValidValidator>(validValue)
+        let input = ValidatedInput<AlwaysSuccessValidator>(validValue)
         
         XCTAssertEqual(input.value, validValue)
         XCTAssertTrue(input.isValid)
         XCTAssertNil(input.errorMessage)
     }
     
+    func test_init_initialisesWithErrorMessageOnValidatorFailure() {
+        let errorMessage = "Any error message."
+        AlwaysFailValidator.errorMessage = errorMessage
+        let input = ValidatedInput<AlwaysFailValidator>("any")
+        
+        XCTAssertNil(input.value)
+        XCTAssertFalse(input.isValid)
+        XCTAssertEqual(input.errorMessage, errorMessage)
+    }
+    
     // MARK: - Helpers
     
-    private final class AlwaysReturnValidValidator: Validator {
+    private func resetErrorMessage() {
+        AlwaysFailValidator.errorMessage = nil
+    }
+    
+    private final class AlwaysSuccessValidator: Validator {
         static var validators: [(String) -> ValidatorResult] {
             [{ _ in return .valid }]
+        }
+    }
+    
+    private final class AlwaysFailValidator: Validator {
+        nonisolated(unsafe) static var errorMessage: String?
+        
+        static var validators: [(String) -> ValidatorResult] {
+            [{ _ in return .invalid(errorMessage) }]
         }
     }
 }
