dc62242c2ff485c35d6aa6384b9b51506bbe5629
tzc1234 <peopli9997@gmail.com>
2025-04-10 16:40:14 +0100

    Render Username error message by minCharacterCount

--- ChattingiOS/View Models/Validating Inputs/Username.swift
+++ ChattingiOS/View Models/Validating Inputs/Username.swift
@@ -11,12 +11,14 @@ enum UsernameValidator: Validator {
     static var validators: [(String) -> ValidatorResult] { [validateNotEmpty, validateCount] }
     
     private static func validateCount(_ name: String) -> ValidatorResult {
-        guard name.count >= 3 else {
-            return .invalid("Name should be 3 or more characters.")
+        guard name.count >= minCharacterCount else {
+            return .invalid("Name should be \(minCharacterCount) or more characters.")
         }
         
         return .valid
     }
+    
+    private static var minCharacterCount: Int { 3 }
 }
 
 typealias Username = ValidatedInput<UsernameValidator>
