ca4d7a0f4cc0b7a8454def36af3e8ca07f9299e6
tzc1234 <peopli9997@gmail.com>
2025-04-10 11:16:18 +0100

    SigInViewModel does not signIn when email/password is empty

--- ChattingiOS/View Models/SignInViewModel.swift
+++ ChattingiOS/View Models/SignInViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 final class SignInViewModel: ObservableObject {
     @Published var emailInput = ""
     @Published var passwordInput = ""
--- ChattingiOSTests/View Models/SignInViewModelTests.swift
+++ ChattingiOSTests/View Models/SignInViewModelTests.swift
@@ -8,17 +8,46 @@
 import XCTest
 @testable import ChattingiOS
 
+@MainActor
 final class SignInViewModelTests: XCTestCase {
     func test_init_doesNotNotifyUserSignInUponCreation() {
-        let spy = UserSignInSpy()
-        _ = SignInViewModel(userSignIn: spy.userSignIn)
+        let (_, spy) = makeSUT()
         
         XCTAssertTrue(spy.loggedParams.isEmpty)
     }
 
+    func test_signIn_doesNotSigInWhenEmailIsEmpty() {
+        let (sut, spy) = makeSUT()
+        
+        sut.emailInput = ""
+        sut.passwordInput = "anyPassword"
+        
+        XCTAssertTrue(spy.loggedParams.isEmpty)
+        XCTAssertFalse(sut.canSignIn)
+    }
+    
+    func test_signIn_doesNotSigInWhenPasswordIsEmpty() {
+        let (sut, spy) = makeSUT()
+        
+        sut.emailInput = "any@email.com"
+        sut.passwordInput = ""
+        
+        XCTAssertTrue(spy.loggedParams.isEmpty)
+        XCTAssertFalse(sut.canSignIn)
+    }
     
     // MARK: - Helpers
     
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: SignInViewModel, spy: UserSignInSpy) {
+        let spy = UserSignInSpy()
+        let sut = SignInViewModel(userSignIn: spy.userSignIn)
+        trackMemoryLeak(spy, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
+        return (sut, spy)
+    }
+    
+    @MainActor
     private final class UserSignInSpy {
         private(set) var loggedParams = [UserSignInParams]()
         private(set) lazy var userSignIn: (UserSignInParams) async throws -> Void = { [weak self] params in
