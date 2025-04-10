5723a7a56c65ac27ea6bfa9bab51c31f32786270
tzc1234 <peopli9997@gmail.com>
2025-04-10 15:45:23 +0100

    Refactor UsernameTests

--- ChattingiOSTests/View Models/Validating Inputs/UsernameTests.swift
+++ ChattingiOSTests/View Models/Validating Inputs/UsernameTests.swift
@@ -11,36 +11,32 @@ import XCTest
 final class UsernameTests: XCTestCase {
     func test_validators_failsWhenNameIsEmpty() {
         let emptyName = ""
-        var results = [ValidatorResult]()
         
-        UsernameValidator.validators.forEach { results.append($0(emptyName)) }
+        let results = UsernameValidator.validators.map { $0(emptyName) }
         
         XCTAssertEqual(results, [.invalid(nil), .invalid("Name should be 3 or more characters.")])
     }
     
     func test_validators_failsWhenNameLessThanThreeCharacters() {
         let lessThanThreeCharactersName = "12"
-        var results = [ValidatorResult]()
         
-        UsernameValidator.validators.forEach { results.append($0(lessThanThreeCharactersName)) }
+        let results = UsernameValidator.validators.map { $0(lessThanThreeCharactersName) }
         
         XCTAssertEqual(results, [.valid, .invalid("Name should be 3 or more characters.")])
     }
     
     func test_validators_succeedsWithThreeCharactersName() {
         let threeCharactersName = "123"
-        var results = [ValidatorResult]()
         
-        UsernameValidator.validators.forEach { results.append($0(threeCharactersName)) }
+        let results = UsernameValidator.validators.map { $0(threeCharactersName) }
         
         XCTAssertEqual(results, [.valid, .valid])
     }
     
     func test_validators_succeedsWithMoreThanThreeCharactersName() {
         let moreThanThreeCharactersName = "1234"
-        var results = [ValidatorResult]()
         
-        UsernameValidator.validators.forEach { results.append($0(moreThanThreeCharactersName)) }
+        let results = UsernameValidator.validators.map { $0(moreThanThreeCharactersName) }
         
         XCTAssertEqual(results, [.valid, .valid])
     }
