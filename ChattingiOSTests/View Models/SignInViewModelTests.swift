aab40e33a5a3f156b0922626c8fd8d238b51a94c
tzc1234 <peopli9997@gmail.com>
2025-04-10 11:36:50 +0100

    Refactor tests

--- ChattingiOSTests/View Models/SignInViewModelTests.swift
+++ ChattingiOSTests/View Models/SignInViewModelTests.swift
@@ -16,37 +16,32 @@ final class SignInViewModelTests: XCTestCase {
         XCTAssertTrue(spy.loggedParams.isEmpty)
     }
 
-    func test_signIn_doesNotSigInWhenEmailIsInvalid() {
-        let (sut, spy) = makeSUT()
+    func test_signIn_doesNotSigInWhenEmailIsInvalid() async {
+        let invalidEmail = ""
+        let (sut, spy) = makeSUT(email: invalidEmail)
         
-        sut.emailInput = ""
-        sut.passwordInput = "anyPassword"
-        sut.signIn()
+        await sut.completeSignIn()
         
         XCTAssertTrue(spy.loggedParams.isEmpty)
         XCTAssertFalse(sut.canSignIn)
     }
     
-    func test_signIn_doesNotSigInWhenPasswordIsInvalid() {
-        let (sut, spy) = makeSUT()
+    func test_signIn_doesNotSigInWhenPasswordIsInvalid() async {
+        let invalidPassword = ""
+        let (sut, spy) = makeSUT(password: invalidPassword)
         
-        sut.emailInput = "any@email.com"
-        sut.passwordInput = ""
-        sut.signIn()
+        await sut.completeSignIn()
         
         XCTAssertTrue(spy.loggedParams.isEmpty)
         XCTAssertFalse(sut.canSignIn)
     }
     
     func test_signIn_passesParamsToUserSignInSuccessfully() async {
-        let (sut, spy) = makeSUT()
         let email = "an@email.com"
         let password = "aPassword"
+        let (sut, spy) = makeSUT(email: email, password: password)
         
-        sut.emailInput = email
-        sut.passwordInput = password
-        sut.signIn()
-        await sut.task?.value
+        await sut.completeSignIn()
         
         XCTAssertEqual(spy.loggedParams, [.init(email: email, password: password)])
         XCTAssertTrue(sut.canSignIn)
@@ -54,10 +49,14 @@ final class SignInViewModelTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath,
+    private func makeSUT(email: String = "an@email.com",
+                         password: String = "aPassword",
+                         file: StaticString = #filePath,
                          line: UInt = #line) -> (sut: SignInViewModel, spy: UserSignInSpy) {
         let spy = UserSignInSpy()
         let sut = SignInViewModel(userSignIn: spy.userSignIn)
+        sut.emailInput = email
+        sut.passwordInput = password
         trackMemoryLeak(spy, file: file, line: line)
         trackMemoryLeak(sut, file: file, line: line)
         return (sut, spy)
@@ -69,5 +68,12 @@ final class SignInViewModelTests: XCTestCase {
         private(set) lazy var userSignIn: (UserSignInParams) async throws -> Void = { [weak self] params in
             self?.loggedParams.append(params)
         }
+    }
+}
+
+private extension SignInViewModel {
+    func completeSignIn() async {
+        signIn()
+        await task?.value
     }
 }
