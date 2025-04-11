46eaab05f227630b4036fd149c0df544d06e677c
tzc1234 <peopli9997@gmail.com>
2025-04-11 10:15:40 +0100

    EmailValidator fails when email is empty

---
+++ ChattingiOSTests/View Models/Validating Inputs/EmailTests.swift
@@ -0,0 +1,20 @@
+//
+//  EmailTests.swift
+//  ChattingiOSTests
+//
+//  Created by Tsz-Lung on 11/04/2025.
+//
+
+import XCTest
+@testable import ChattingiOS
+
+final class EmailTests: XCTestCase {
+    func test_validators_failsWhenEmailIsEmpty() {
+        let emptyEmail = ""
+        
+        let results = EmailValidator.validators.map { $0(emptyEmail) }
+        
+        XCTAssertEqual(results, [.invalid(nil), .invalid("Email format is not correct.")])
+    }
+
+}
