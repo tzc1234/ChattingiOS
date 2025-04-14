dedd888dc996bd6d0da1771c37f201ccc89fdb8b
tzc1234 <peopli9997@gmail.com>
2025-04-14 14:06:48 +0100

    ContactListViewModel init does not notify collaborators upon creation

---
+++ ChattingiOSTests/View Models/ContactListViewModelTests.swift
@@ -0,0 +1,57 @@
+//
+//  ContactListViewModelTests.swift
+//  ChattingiOSTests
+//
+//  Created by Tsz-Lung on 14/04/2025.
+//
+
+import XCTest
+@testable import ChattingiOS
+
+@MainActor
+final class ContactListViewModelTests: XCTestCase {
+    func test_init_doesNotNotifyCollaboratorsUponCreation() {
+        let (_, spy) = makeSUT()
+        
+        XCTAssertTrue(spy.messages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentUserID: Int = 99,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: ContactListViewModel, spy: CollaboratorsSpy) {
+        let spy = CollaboratorsSpy()
+        let sut = ContactListViewModel(
+            currentUserID: currentUserID,
+            getContacts: spy,
+            blockContact: spy,
+            unblockContact: spy)
+        trackMemoryLeak(spy, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
+        return (sut, spy)
+    }
+    
+    @MainActor
+    private final class CollaboratorsSpy: GetContacts, BlockContact, UnblockContact {
+        enum Message {
+            case get(with: GetContactsParams)
+            case block(for: Int)
+            case unblock(for: Int)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func get(with params: GetContactsParams) async throws(UseCaseError) -> [Contact] {
+            fatalError()
+        }
+        
+        func block(for contactID: Int) async throws(UseCaseError) -> Contact {
+            fatalError()
+        }
+        
+        func unblock(for contactID: Int) async throws(UseCaseError) -> Contact {
+            fatalError()
+        }
+    }
+}
