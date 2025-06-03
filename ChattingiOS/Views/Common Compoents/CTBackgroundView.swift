52106edee5688fab11d7388baf256c1f5fc48fe5
tzc1234 <peopli9997@gmail.com>
2025-06-03 08:37:59 +0100

    Remove animation from CTBackgroundView

--- ChattingiOS/Views/Common Compoents/CTBackgroundView.swift
+++ ChattingiOS/Views/Common Compoents/CTBackgroundView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct CTBackgroundView: View {
     @EnvironmentObject private var style: ViewStyleManager
-    @State private var isAnimating: Bool = false
     
     var body: some View {
         style.common.background
             .ignoresSafeArea()
-            .hueRotation(.degrees(isAnimating ? 10 : 0))
-            .animation(Animation.easeInOut(duration: 3).repeatForever(autoreverses: true), value: isAnimating)
-            .onAppear { isAnimating = true }
     }
 }
