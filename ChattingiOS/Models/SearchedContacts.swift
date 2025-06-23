f16b2476e234e0f9bfa0d61f853d75635b9da31f
tzc1234 <peopli9997@gmail.com>
2025-06-23 09:50:13 +0100

    Add total to SearchedContacts

--- ChattingiOS/Models/SearchedContacts.swift
+++ ChattingiOS/Models/SearchedContacts.swift
@@ -10,4 +10,5 @@ import Foundation
 struct SearchedContacts {
     let contacts: [Contact]
     let hasMore: Bool
+    let total: Int
 }
--- ChattingiOS/Networkings/Mappers/Responses/SearchContactsResponse.swift
+++ ChattingiOS/Networkings/Mappers/Responses/SearchContactsResponse.swift
@@ -10,13 +10,15 @@ import Foundation
 struct SearchContactsResponse: Response {
     let contacts: [ContactResponse]
     let hasMore: Bool
+    let total: Int
     
     enum CodingKeys: String, CodingKey {
         case contacts
         case hasMore = "has_more"
+        case total
     }
     
     var toModel: SearchedContacts {
-        SearchedContacts(contacts: contacts.map(\.toModel), hasMore: hasMore)
+        SearchedContacts(contacts: contacts.map(\.toModel), hasMore: hasMore, total: total)
     }
 }
