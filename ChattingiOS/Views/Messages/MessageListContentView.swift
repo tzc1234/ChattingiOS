9f66951eb117f2211aca5535296a44f71bee7a7c
tzc1234 <peopli9997@gmail.com>
2025-06-25 21:33:48 +0100

    SmartLinkText

--- ChattingiOS/Views/Messages/MessageListContentView.swift
+++ ChattingiOS/Views/Messages/MessageListContentView.swift
@@ -332,7 +332,7 @@ struct MessageBubbleContent: View {
     let message: DisplayedMessage
     
     var body: some View {
-        Text(message.text)
+        SmartLinkText(text: message.text)
             .font(.callout)
             .italic(message.isDeleted)
             .foregroundColor(style.message.bubble.foregroundColor(isMine: isMine))
@@ -406,6 +406,41 @@ struct MessageBubble: View {
             if !isMine { Spacer() }
         }
         .id(message.text)
+    }
+}
+
+struct SmartLinkText: View {
+    let text: String
+    
+    var body: some View {
+        Text(makeAttributedString())
+            .environment(\.openURL, OpenURLAction { url in
+                print("Intercepted URL: \(url)")
+                return .discarded
+            })
+    }
+    
+    private func makeAttributedString() -> AttributedString {
+        var attributedString = AttributedString(text)
+        
+        let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        let range = NSRange(location: 0, length: text.utf16.count)
+        
+        detector?.enumerateMatches(in: text, options: [], range: range) { match, _, _ in
+            guard let match,
+                  let range = Range(match.range, in: text),
+                  let attributedRange = Range(range, in: attributedString),
+                  let url = match.url else {
+                return
+            }
+            
+            attributedString[attributedRange].foregroundColor = .orange
+            attributedString[attributedRange].underlineStyle = .single
+            attributedString[attributedRange].font = .callout.weight(.semibold)
+            attributedString[attributedRange].link = url
+        }
+        
+        return attributedString
     }
 }
 
